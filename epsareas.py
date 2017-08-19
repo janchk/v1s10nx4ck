7e8fa03f073b5a68960afbfb87fc40aa9f66ab6f
jan <janchk@gmail.com>
2017-08-20 00:24:31 +0700

    worked but not preciesly

--- epsareas.py
+++ epsareas.py
@@ -1,27 +1,47 @@
 import cv2
 import numpy as np
+import sys
 
+green_up = np.array([147, 252, 101], dtype='uint8')
+green_lw = np.array([47, 148, 3], dtype='uint8')
 
-def epsareas_func(contours, s_frame):
+def epsareas_func(contours, s_frame, frame_number):
+
+    black = np.zeros([1080, 1920, 1], dtype="uint8") # для фильтра
     # Определяем окрестность
     epsareas = [cv2.boundingRect(a) for a in contours]
 
     # Убираем слишком маленькие области
     areas_space = [cv2.contourArea(c) for c in contours]
-    ind_big_spaces = [i for i, x in enumerate(areas_space) if x > 15]
+    ind_big_spaces = [i for i, x in enumerate(areas_space) if x > 40]
     epsareas = [epsareas[ind] for ind in ind_big_spaces]
 
     # Чистим входящие маленькие области
-    for i in epsareas:
-        for j in epsareas:
-            xi, yi, wi, hi = i
-            xj, yj, wj, hj = j
-            if (xi-10) < xj <= (xi+wi+10) and (yi-10) < yj <= (yi + 4*hi + 5):
-                epsareas.remove(j)
+    # for i in epsareas:
+    #     for j in epsareas:
+    #         xi, yi, wi, hi = i
+    #         xj, yj, wj, hj = j
+    #         if (xi-10) < xj <= (xi+wi+10) and (yi-10) < yj <= (yi + 4*hi + 5):
+    #             epsareas.remove(j)
+
 
     for epsarea in epsareas:
+
         x, y, w, h = epsarea
-        cv2.rectangle(s_frame, (x - 10, y - 10), (x + w + 10, y + 4 * h + 5), (0, 0, 255), 2)  # красный
-        cv2.rectangle(s_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # зелёный
-        cv2.imshow("Show", cv2.resize(s_frame, (800, 600)))
+        adv_red_mask = cv2.rectangle(black, (x - 10, y - 10), (x + w + 10, y + 3 * h + 5), (255, 255, 255), -1)  # белая область
+        # cv2.rectangle(s_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)  # зелёный квадрат
+        # mask_white = cv2.inRange(smimg, np.array([254, 254, 254], dtype="uint8"), np.array([255, 255, 255], dtype="uint8"))
+        # cv2.imshow("Show", cv2.resize(s_frame, (800, 600)))
+        masked = cv2.bitwise_and(s_frame, s_frame, mask=adv_red_mask)
+        grn_masked = cv2.inRange(masked, green_lw, green_up)
+
+
+        cv2.imshow("Masked", cv2.resize(masked, (800,600)))
+        # cv2.imshow("GRMasked", grn_masked)
+        ret, thresh = cv2.threshold(grn_masked, 127, 255, cv2.THRESH_BINARY)
+        _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        if len(contours) > 0:
+            print(frame_number)
+            sys.exit()
+
         # cv2.imshow("LZ_Show", cv2.resize(frame, (800, 600)))
--- main.py
+++ main.py
@@ -3,32 +3,31 @@ import cv2
 import copy
 from epsareas import epsareas_func
 
-file = "C:/Users/janch/Desktop/validationset/akn.014.044.left.avi"
+file = "C:/Users/janch/Desktop/validationset/akn.099.060.left.avi"
 cap = cv2.VideoCapture(file)
 # cap_lz = cv2.VideoCapture(file)
-ret1, frame = cap.read()
+ret1, s_frame = cap.read()  # s_frame frame for show
 frame_count = 0
 kernel = np.ones((5, 5), np.uint8)
+red_mask_mass = []
+# red_mask1 = np.array([1])
+# red_mask2 = np.array([1])
 while True:
 
     frame_count += 1
     if frame_count % 1 == 0:
-        ret1, frame = cap.read()
-    s_frame = copy.copy(frame)
+        ret1, s_frame = cap.read()
+    frame = copy.copy(s_frame)[0:640, 0:1920]  # кадр для рисования, пусть будет
 
     # Покадровое считывание
     # ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # границы красного цвета BGR
     lower_red = np.array([0, 0, 110], dtype="uint8")
     upper_red = np.array([100, 100, 255], dtype="uint8")
 
-
-    # границы красного цвета HSV
-    # lower_red_hsv = np.array([150, 0, 0], np.uint8)
-    # upper_red_hsv = np.array([250, 155, 255], np.uint8)
 
     lower_black = np.array([0, 0, 0], dtype="uint16")
     upper_black = np.array([40, 40, 40], dtype="uint16")
@@ -42,14 +41,29 @@ while True:
     mask_red1 = cv2.morphologyEx(mask_red1, cv2.MORPH_CLOSE, kernel)
     # mask_red1 = cv2.dilate(mask_red1, kernel, iterations=1)
 
+    # магия с последовательным изчезновением красного
+    if len(red_mask_mass) < 6:
+        red_mask_mass.append(mask_red1)
+    else:
+        red_mask_mass.pop(0)
+        red_mask_mass.append(mask_red1)
+
+    sum_rmask = mask_red1
+    if len(red_mask_mass) > 0:
+        for rmask in red_mask_mass:
+            sum_rmask = cv2.addWeighted(rmask, 0.5, sum_rmask, 1, 1)
+    cv2.imshow('summ_rmask', sum_rmask)
+
+    # наложение масок на изображение. Совсем не обязательно, но наглядно.
     black = cv2.bitwise_and(frame, frame, mask=mask_black)
-    red = cv2.bitwise_and(frame, frame, mask=mask_red1)
+    red = cv2.bitwise_and(frame, frame, mask=sum_rmask)
 
 
 
     # порог
-    ret, thresh = cv2.threshold(mask_red1, 127, 255, cv2.THRESH_TOZERO)
-    _, contours, hierarchy = cv2.findContours(mask_red1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # разобраться
+    ret, thresh = cv2.threshold(sum_rmask, 127, 255, cv2.THRESH_BINARY)
+    cv2.imshow("Thresh", cv2.resize(thresh, (800, 600)))
+    _, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)  # разобраться
 
     # поиск индекса с наибольшей площадью
     # areas_space = [cv2.contourArea(c) for c in contours]
@@ -58,7 +72,7 @@ while True:
     #---------------------------------------------------
 
     if frame_count % 3 == 0:
-        epsareas_func(contours, s_frame)
+        epsareas_func(contours, s_frame, frame_count)
 
 
     # for epsarea in contours:
@@ -97,18 +111,18 @@ while True:
     # cv2.imshow("Show", bgray)
     # Наложение маски
 
-    rs = cv2.resize(gray, (800, 600))
-    rs2 = cv2.resize(mask_red, (800, 600))
+    # rs = cv2.resize(gray, (800, 600))
+    # rs2 = cv2.resize(mask_red1, (800, 600))
     rs3 = cv2.resize(thresh, (800, 600))
-    rs4 = cv2.resize(red, (800, 600))
-    rs5 = cv2.resize(mask_black, (800, 600))
+    # rs4 = cv2.resize(red, (800, 600))
+    # rs5 = cv2.resize(mask_black, (800, 600))
 
     # Вывод
 
     # cv2.imshow('gray', rs)
-    cv2.imshow('mask_red', rs2)
+    # cv2.imshow('mask_red1', rs2)
     # cv2.imshow('thresh', rs3)
-    cv2.imshow('red', rs4)
+    # cv2.imshow('red', rs4)
     # cv2.imshow('black', rs5)
 
     # Delay между кадрами в мс
